0143d9168966a066f0b8e4e22cd59158672ee9cf
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-18 11:54:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
+
+
+def page_2():
+    st.title("Page 2")
+
+pg = st.navigation(["page_1.py", page_2])
+pg.run()
+
 st.title("🎈 Kalkulator PLI")
 st.header(
     "Menghitung hasil operasi aritmatika sederhana", divider=True
 )
+number1 = st.number_input("Masukkan angka 1")
+number2 = st.number_input("Masukkan angka 2")
