8422e4e93650ba3da79e49016dea51df621999eb
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-18 11:42:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Kalkulator PLI")
 st.header(
-    "Menghitung hasil operasi aritmatika sederhana"
+    "Menghitung hasil operasi aritmatika sederhana", divider=True
 )
