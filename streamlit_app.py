13093db074447b16081027423a9ba075968eab4b
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-18 11:58:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
 
-
-def page_2():
-    st.title("Page 2")
-
-pg = st.navigation(["page_1.py", page_2])
-pg.run()
 '''
 st.title("🎈 Kalkulator PLI")
 st.header(
@@ -14,4 +8,6 @@ st.header(
 )
 number1 = st.number_input("Masukkan angka 1")
 number2 = st.number_input("Masukkan angka 2")
-'''
+if st.button("tambah"):
+    st.header(number1+number2)
+
