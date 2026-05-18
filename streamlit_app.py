37211ecc5eef4fec3e9726d62495de72236b1da7
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-18 12:05:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ st.header(
 )
 number1 = st.number_input("Masukkan angka 1")
 number2 = st.number_input("Masukkan angka 2")
+st.button("+"), st.button("-"),  st.button("x")= st.columns(3)
+st.button(":"), st.button("^"), st.button("reset")=st.columns(3)
 if st.button("+"):
     st.header(number1+number2)
 elif st.button("-"):
